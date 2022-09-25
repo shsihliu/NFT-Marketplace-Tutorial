4bd36fa0e58054a18b8ecb5168a1ff2e5a6bb83d
shsihliu <shishiliu730@gmail.com>
2022-09-25 10:07:30 +0000

    shishi week 07

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,29 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
+const { TASK_COMPILE_SOLIDITY_GET_SOLC_BUILD } = require("hardhat/builtin-tasks/task-names");
+const path = require("path");
+
+subtask(TASK_COMPILE_SOLIDITY_GET_SOLC_BUILD, async (args, hre, runSuper) => {
+  if (args.solcVersion === "0.8.4") {
+    const compilerPath = path.join(__dirname, "node_modules/solc/soljson.js");
+
+    return {
+      compilerPath,
+      isSolcJs: true, // if you are using a native compiler, set this to false
+      version: args.solcVersion,
+      // this is used as extra information in the build-info files, but other than
+      // that is not important
+      // longVersion: "0.8.5-nightly.2021.5.12+commit.98e2b4e5"
+    }
+  }
+
+  // we just use the default subtask if the version is not 0.8.5
+  return runSuper();
+})
+
 const fs = require('fs');
 // const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+require('dotenv').config();
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
@@ -12,22 +34,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 });
 
 module.exports = {
-  defaultNetwork: "hardhat",
   networks: {
-    hardhat: {
-      chainId: 1337
-    },
-    mumbai: {
-      url: `https://polygon-mumbai.g.alchemy.com/v2/nAhiCHKvZkhkp4A7PkkCIBON0-BXW26d`,
-      //accounts: [process.env.privateKey]
-    },
-    matic: {
-      url: "https://polygon-mainnet.g.alchemy.com/v2/nAhiCHKvZkhkp4A7PkkCIBON0-BXW26d",
-      //accounts: [process.env.privateKey]
-    },
     goerli: {
       url: process.env.REACT_APP_ALCHEMY_API_URL,
-      accounts: [ process.env.REACT_APP_PRIVATE_KEY ]
+      accounts: [process.env.REACT_APP_PRIVATE_KEY]
     }
   },
   solidity: {
